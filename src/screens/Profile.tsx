8cecb3839b428387b48a5ec3bf94d48b5485e1ec
PauloAbrahao <ab.cod@outlook.com>
2024-05-02 20:30:21 -0300

    feat: handling user photo and inputs

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -9,15 +9,58 @@ import {
   Skeleton,
   Text,
   Heading,
-  useTheme,
+  useToast,
 } from "native-base";
 import {useState} from "react";
-import {TouchableOpacity} from "react-native";
+import {Alert, TouchableOpacity} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 
 const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState(
+    "https://github.com/PauloAbrahao.png"
+  );
+
+  const toast = useToast();
+
+  async function handleUserPhotoSelected() {
+    setPhotoIsLoading(true);
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = (await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri
+        )) as FileSystem.FileInfo;
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+          return toast.show({
+            title: "Essa imagem é muito grande. Escolha uma de até 5MB.",
+            placement: "bottom",
+            bgColor: "red.500",
+          });
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (err) {
+      console.log("err", err);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -38,13 +81,13 @@ export function Profile() {
             />
           ) : (
             <UserPhoto
-              source={{uri: "https://github.com/PauloAbrahao.png"}}
+              source={{uri: userPhoto}}
               size={PHOTO_SIZE}
               alt="Foto do usuário"
             />
           )}
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelected}>
             <Text
               color="green.500"
               fontWeight="bold"
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -6,13 +6,20 @@ import BackgroundImg from "@assets/background.png";
 import {Input} from "@components/Input";
 import {Button} from "@components/Button";
 import {useNavigation} from "@react-navigation/native";
+import {useForm, Controller} from "react-hook-form";
 
 export function SignUp() {
+  const {control, handleSubmit} = useForm();
   const navigation = useNavigation();
 
   function handleGoBack() {
     navigation.goBack();
   }
+
+  function handleSignUp(data: any) {
+    console.log({ data })
+  }
+
   return (
     <ScrollView
       contentContainerStyle={{flexGrow: 1}}
@@ -40,16 +47,57 @@ export function SignUp() {
             Crie sua conta
           </Heading>
 
-          <Input placeholder="Nome" />
-
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
+          <Controller
+            control={control}
+            name="name"
+            render={({field: {onChange, value}}) => (
+              <Input placeholder="Nome" onChangeText={onChange} value={value} />
+            )}
           />
-          <Input placeholder="Senha" secureTextEntry />
 
-          <Button title="Criar e acessar" />
+          <Controller
+            control={control}
+            name="email"
+            render={({field: {onChange, value}}) => (
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="password"
+            render={({field: {onChange, value}}) => (
+              <Input
+                placeholder="Senha"
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="password_confirm"
+            render={({field: {onChange, value}}) => (
+              <Input
+                placeholder="Confirme sua senha"
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+                onSubmitEditing={handleSubmit(handleSignUp)}
+                returnKeyType="send"
+              />
+            )}
+          />
+
+          <Button title="Criar e acessar" onPress={handleSubmit(handleSignUp)} />
         </Center>
 
         <Button
